40171a85646f1192e9eee0645faf81ac3bbf96e7
James Ansley <31982662+James-Ansley@users.noreply.github.com>
2023-01-04 22:07:13 +1300

    added stack frame indents

--- src/pytubemusic/logutils.py
+++ src/pytubemusic/logutils.py
@@ -1,7 +1,12 @@
 import functools
 import logging
+from textwrap import indent
 
 logger = logging.getLogger("pytubemusic")
+
+indent_width = 2
+
+stack_depth = 0
 
 
 def log_call(
@@ -12,11 +17,21 @@ def log_call(
     def decorator(func):
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
+            global stack_depth
+            space = " " * indent_width * stack_depth
             if on_enter is not None:
-                logger.log(level, on_enter.format(*args, **kwargs))
+                logger.log(
+                    level,
+                    indent(on_enter, space).format(*args, **kwargs)
+                )
+            stack_depth += 1
             result = func(*args, **kwargs)
+            stack_depth -= 1
             if on_exit is not None:
-                logger.log(level, on_exit.format(*args, **kwargs))
+                logger.log(
+                    level,
+                    indent(on_exit, space).format(*args, **kwargs)
+                )
             return result
 
         return wrapper
