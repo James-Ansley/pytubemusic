7f2ba90a8cead16b7b7a981086d2154f422906e0
James Ansley <31982662+James-Ansley@users.noreply.github.com>
2023-01-04 22:08:04 +1300

    created album/track classes – added support for playlists

--- src/pytubemusic/__main__.py
+++ src/pytubemusic/__main__.py
@@ -1,12 +1,11 @@
 import logging
-import os
 import tomllib
 from pathlib import Path
 
 import typer
 from typer import Argument, Option
 
-from .audio import Audio
+from .audio import Album, Track
 from .logutils import log_call
 
 app = typer.Typer(add_completion=False)
@@ -21,11 +20,11 @@ logger.addHandler(logging.StreamHandler())
     help="Gets album tracks as individual files "
          "from the video associated with the album_data file",
 )
-@log_call(on_enter="Getting album from {album_data}", on_exit="Done!")
-def album(
+@log_call(on_enter="Building album from '{album_data}'", on_exit="DONE")
+def make_album(
         album_data: Path = Argument(..., help="The album data"),
         out: Path = Option(
-            None,
+            Path("."),
             "--out", "-o",
             help="The directory album tracks will be written to. "
                  "Defaults to the album name",
@@ -33,13 +32,8 @@ def album(
 ):
     with open(album_data, "rb") as f:
         album_data = tomllib.load(f)
-    audio = Audio.from_url(**album_data)
-    tracks = audio.split_to_tracks()
-    if out is None:
-        out = Path(audio.metadata["album"])
-    os.makedirs(out, exist_ok=True)
-    for track in tracks:
-        track.export(out)
+    album = Album.from_url(**album_data)
+    album.export(out)
 
 
 @app.command(
@@ -47,8 +41,8 @@ def album(
     help="Gets a single audio file from the "
          "video associated with the track_data file",
 )
-@log_call(on_enter="Getting track from {track_data}", on_exit="Done!")
-def track(
+@log_call(on_enter="Building album from '{track_data}'", on_exit="DONE")
+def make_track(
         track_data: Path = Argument(..., help="The track data"),
         out: Path = Option(
             Path("."),
@@ -59,9 +53,28 @@ def track(
 ):
     with open(track_data, "rb") as f:
         track_data = tomllib.load(f)
-    audio = Audio.from_url(**track_data)
-    os.makedirs(out, exist_ok=True)
-    audio.export(out)
+    track = Track.from_url(**track_data)
+    track.export(out)
+
+
+@app.command(
+    "playlist",
+    help="Gets tracks from the videos associated with the playlist",
+)
+@log_call(on_enter="Building album from '{playlist_data}'", on_exit="DONE")
+def make_playlist_album(
+        playlist_data: Path = Argument(..., help="The track data"),
+        out: Path = Option(
+            Path("."),
+            "--out", "-o",
+            help="The directory album tracks will be written to. "
+                 "Defaults to the cwd",
+        )
+):
+    with open(playlist_data, "rb") as f:
+        playlist_data = tomllib.load(f)
+    playlist = Album.from_playlist(**playlist_data)
+    playlist.export(out)
 
 
 app()
--- src/pytubemusic/audio.py
+++ src/pytubemusic/audio.py
@@ -1,72 +1,136 @@
+import os
+from collections.abc import Mapping
+from datetime import timedelta
 from itertools import pairwise
-from pathlib import Path
+from pathlib import PurePath
 from tempfile import NamedTemporaryFile
 from typing import Self
 
 from dateutil.parser import parse
 from pydub import AudioSegment
-from pytube import YouTube
+from pytube import Playlist, YouTube
 
 from pytubemusic.logutils import log_call
 
 
-class Audio:
-    def __init__(self, audio_data: AudioSegment, bitrate: int, metadata: dict):
-        self._audio_data = audio_data
-        self.bitrate = bitrate
+class Album:
+    def __init__(self, tracks: list["Track"], metadata: Mapping[str, str]):
+        self.tracks = tracks
         self.metadata = metadata
 
     @classmethod
-    @log_call(on_enter="Downloading Audio \"{metadata[name]}\" from {url}")
-    def from_url(cls, *, url, metadata) -> Self:
+    @log_call(on_enter="Downloading '{metadata[album]}' from {url}")
+    def from_url(
+            cls,
+            url: str,
+            track_data: list[Mapping],
+            metadata: Mapping[str, str],
+    ) -> Self:
+        audio = Audio.from_stream(url)
+        track_data = Track.normalised_data(track_data, audio.duration.seconds)
+        tracks = [Track.from_audio(audio, data) for data in track_data]
+        return cls(tracks, metadata)
+
+    @classmethod
+    @log_call(on_enter="Downloading '{metadata[album]}' from {url}")
+    def from_playlist(cls, url: str, metadata: Mapping[str, str]):
+        playlist = Playlist(url)
+        tracks = [
+            Track.from_url(
+                url=video.watch_url,
+                metadata={"track": i, "title": video.title}
+            )
+            for i, video in enumerate(playlist.videos, start=1)
+        ]
+        return cls(tracks, metadata)
+
+    @log_call(on_enter="Exporting album '{0.metadata[album]}'")
+    def export(self, root: PurePath = PurePath(".")):
+        root = root / self.metadata["album"]
+        os.makedirs(root, exist_ok=True)
+        for track in self.tracks:
+            track.export(root, self.metadata)
+
+
+class Track:
+    def __init__(self, audio: "Audio", metadata: Mapping[str, str]):
+        self._audio = audio
+        self.metadata = metadata
+
+    @classmethod
+    @log_call(on_enter="Downloading '{metadata[title]}' from {url}")
+    def from_url(
+            cls,
+            *,
+            url: str,
+            metadata: Mapping[str, str],
+            start: int = None,
+            end: int = None,
+    ) -> Self:
+        audio = Audio.from_stream(url).snip(start, end)
+        return cls(audio, metadata)
+
+    @classmethod
+    def from_audio(cls, audio, track_data) -> Self:
+        track_audio = audio.snip(track_data["start"], track_data["end"])
+        return cls(track_audio, track_data["metadata"])
+
+    @staticmethod
+    def normalised_data(track_data: list[Mapping], end: int):
+        track_data = track_data + [{"start": to_timestamp(end)}]
+        for i, (t1, t2) in enumerate(pairwise(track_data), start=1):
+            yield t1 | {
+                "start": to_delta(t1["start"]),
+                "end": to_delta(t1.get("end", t2["start"])),
+                "metadata": {**t1.get("metadata", {}), "track": i},
+            }
+
+    @log_call(
+        on_enter="Exporting track {0.metadata[track]}: {0.metadata[title]}")
+    def export(self, root: PurePath = PurePath("."), metadata: Mapping = None):
+        metadata = {} if metadata is None else metadata
+        root = root / (self.metadata["title"].replace("/", "\u2044") + ".mp3")
+        with open(root, "wb") as f:
+            self._audio.export(f, metadata | self.metadata)
+
+
+class Audio:
+    def __init__(self, audio_data: AudioSegment, bitrate: int):
+        self._audio_data = audio_data
+        self.bitrate = bitrate
+
+    @classmethod
+    def from_stream(cls, url: str) -> Self:
         audio_file = NamedTemporaryFile("wb")
         raw_audio = YouTube(url).streams.get_audio_only()
         raw_audio.stream_to_buffer(audio_file)
         audio_data = AudioSegment.from_file(audio_file.name)
-        return cls(audio_data, raw_audio.bitrate, metadata)
+        return cls(audio_data, raw_audio.bitrate)
+
+    def snip(self, start: int = None, end: int = None) -> Self:
+        start = 0 if start is None else start
+        end = self._audio_data.duration_seconds if end is None else end
+        start, end = start * 1000, end * 1000
+        return type(self)(self._audio_data[start:end], self.bitrate)
 
     @property
-    def microseconds(self) -> int:
-        return self._audio_data.duration_seconds * 1000
+    def duration(self):
+        return timedelta(seconds=self._audio_data.duration_seconds)
 
-    @log_call(on_enter="Splitting \"{0.metadata[name]}\" to Tracks")
-    def split_to_tracks(self):
-        tracks = self._tracks_with_timestamp_ranges()
-        self.metadata.pop("tracks")
-        new_tracks = []
-        for i, track in enumerate(tracks, start=1):
-            new_tracks.append(
-                Audio(
-                    self._audio_data[track["from"]:track["to"]],
-                    self.bitrate,
-                    self.metadata | {
-                        "track": f"{i}/{len(tracks)}",
-                        "name": track["name"],
-                    },
-                )
-            )
-        return new_tracks
-
-    @log_call(on_enter="Writing {0.metadata[name]}")
-    def export(self, out: Path):
-        track_file = out / (self.metadata["name"] + ".mp3")
-        with open(track_file, "wb") as f:
-            self._audio_data.export(
-                f,
-                format="mp3",
-                tags=self.metadata,
-                parameters=["-b:a", f"{self.bitrate}"]
-            )
-
-    def _tracks_with_timestamp_ranges(self):
-        tracks = self.metadata["tracks"]
-        tracks = [{**t, "time": to_delta(t["time"])} for t in tracks]
-        times = pairwise((*(t["time"] for t in tracks), self.microseconds))
-        return [{**track, "from": t1, "to": t2}
-                for track, (t1, t2) in zip(tracks, times)]
+    def export(self, f, metadata):
+        self._audio_data.export(
+            f,
+            format="mp3",
+            tags=metadata,
+            parameters=["-b:a", f"{self.bitrate}"]
+        )
 
 
 def to_delta(timestamp):
     template = "00:00:00"
     timestamp = template[:-len(timestamp)] + timestamp
-    return (parse(timestamp) - parse(template)).seconds * 1000
+    return (parse(timestamp) - parse(template)).seconds
+
+
+def to_timestamp(seconds):
+    return f"{seconds // 3600}:{(seconds // 60) % 60}:{seconds % 60}"
